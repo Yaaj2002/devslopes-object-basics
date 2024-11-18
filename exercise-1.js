557392a9caadcc4f7743d33c46d9744adbd8fb1f
Justin Yang <yangjustin90@gmail.com>
2024-11-18 08:49:50 -0500

    First object created

--- exercise-1.js
+++ exercise-1.js
@@ -4,3 +4,10 @@ Start with exercise-1.md if you haven't read it!
 */
 
 // YOUR WORK GOES HERE //
+const bubblys = {
+    name: 'Bubblys',
+    cuisine: 'Asian Fusion',
+    numberOfStars: 4.7,
+    favorited: true,
+}
+
